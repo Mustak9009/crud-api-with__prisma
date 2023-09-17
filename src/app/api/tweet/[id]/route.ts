a3dc1c613cc2ac7b4934c37ad060724e525c39b4
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2023-09-17 14:41:57 +0530

    add

--- src/app/api/tweet/[id]/route.ts
+++ src/app/api/tweet/[id]/route.ts
@@ -14,3 +14,28 @@ export async function GET(req:NextRequest,params:{params:{id:string}}){
         await prisma.$disconnect();
     }
 }
+export async function PUT(req:NextRequest,params:{params:{id:string}}){
+    try{
+        const reqBody = await req.json();
+        const {tweet} = reqBody;
+        const updatedTweet = await prisma.tweet.update({data:{tweet:tweet},where:{id:params.params.id}});
+        return NextResponse.json({updatedTweet},{status:200});
+    }catch(err){
+        console.log(err);
+        return NextResponse.json({error:err},{status:500});
+    }finally{
+        await prisma.$disconnect();
+    }
+}
+
+export async function  DELETE(req:NextRequest,params:{params:{id:string}}){
+    try{
+        const deletedTweet = await prisma.tweet.delete({where:{id:params.params.id}});
+        return NextResponse.json({deletedTweet},{status:200});
+    }catch(err){
+        console.log(err);
+        return NextResponse.json({error:err},{status:500});
+    }finally{
+        await prisma.$disconnect();
+    }
+}
